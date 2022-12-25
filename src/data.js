0695dcd703c1235b17ac7f7f9b4b1a93762f7b67
An Cao <kcj.pop.com@gmail.com>
2022-12-25 12:12:51 +0200

    Show classes of a ns
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/data.js
+++ src/data.js
@@ -3,3 +3,17 @@ import api from './api.json'
 export function getVersion() {
   return api.version
 }
+
+export function getNamespaces() {
+  return api.namespaces
+}
+
+export function getNamespace(ns) {
+  return {
+    name: ns,
+    classes: api.classes[ns],
+    enums: api.enums[ns],
+    typeAliases: api.typeAliases[ns],
+    defs: api.defs[ns],
+  }
+}
